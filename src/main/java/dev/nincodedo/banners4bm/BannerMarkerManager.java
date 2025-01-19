04d2681969b478246a24d181f05222ffa43fe67e
RealMuffinTime <realmuffintime@gmail.com>
2025-01-20 00:11:06 +0100

    allow any dimension fix block breaking not removing banner known issue: breaking support block of banner does not remove it

--- src/main/java/dev/nincodedo/banners4bm/BannerMarkerManager.java
+++ src/main/java/dev/nincodedo/banners4bm/BannerMarkerManager.java
@@ -2,15 +2,16 @@ package dev.nincodedo.banners4bm;
 
 import de.bluecolored.bluemap.api.BlueMapAPI;
 import de.bluecolored.bluemap.api.BlueMapMap;
+import de.bluecolored.bluemap.api.BlueMapWorld;
 import de.bluecolored.bluemap.api.gson.MarkerGson;
 import de.bluecolored.bluemap.api.markers.MarkerSet;
 import de.bluecolored.bluemap.api.markers.POIMarker;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BannerBlockEntity;
-import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,63 +24,55 @@ public class BannerMarkerManager {
 
     private static final Logger LOGGER = LoggerFactory.getLogger("BannerMarkerManager");
 
-    private final String markerJsonFileName = FabricLoader.getInstance().getConfigDir().resolve( "banners4bm/maps/overworld.json" ).toString();
-    private final String markerSetLabel = "Map Banners";
-    private final String bannerMarkerSetId = "overworldmapbanners";
+    private final String bannerMarkerSetId = "banners4bm";
 
-    public void loadMarkers(ServerWorld overworld) {
-        MarkerSet bannerMarkerSet = getMarkerSet();
-        var optionalApi = BlueMapAPI.getInstance();
-        if (bannerMarkerSet == null || optionalApi.isEmpty()) {
-            return;
-        }
-        var api = optionalApi.get();
-
-        api.getWorld(overworld).ifPresent(blueMapWorld -> blueMapWorld.getMaps().forEach(blueMapMap -> blueMapMap.getMarkerSets().put(bannerMarkerSetId, bannerMarkerSet)));
-    }
-
-    private MarkerSet getMarkerSet() {
-        File markerFile = new File(markerJsonFileName);
-        if (markerFile.exists()) {
-            try (FileReader reader = new FileReader(markerJsonFileName)) {
-                return MarkerGson.INSTANCE.fromJson(reader, MarkerSet.class);
-            } catch (IOException ex) {
-                // handle io-exception
-                ex.printStackTrace();
-            }
-        } else {
-            return MarkerSet.builder().label(markerSetLabel).defaultHidden(false).toggleable(true).build();
-        }
-        return null;
-    }
-
-    public void saveMarkers() {
-        BlueMapAPI.getInstance().ifPresent(blueMapAPI -> blueMapAPI.getMaps().forEach(blueMapMap -> blueMapMap.getMarkerSets().forEach((id, markerSet) -> {
-            if (id != null && id.equals(bannerMarkerSetId)) {
-                try (FileWriter writer = new FileWriter(markerJsonFileName)) {
-                    MarkerGson.INSTANCE.toJson(markerSet, writer);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
+    public void loadMarkerSets() {
+        BlueMapAPI.getInstance().ifPresent(api -> {
+            for (BlueMapWorld world : api.getWorlds()) {
+                // Not the nicest with the names
+                String fileName = FabricLoader.getInstance().getConfigDir().resolve(String.format("banners4bm/maps/%s.json", world.getId().replace("#minecraft:", "_").replace("_overworld", ""))).toString();
+                File markerSetsFile = new File(fileName);
+                if (markerSetsFile.exists()) {
+                    try (FileReader reader = new FileReader(fileName)) {
+                        world.getMaps().forEach(map -> map.getMarkerSets().put(bannerMarkerSetId, MarkerGson.INSTANCE.fromJson(reader, MarkerSet.class)));
+                    } catch (IOException ex) {
+                        LOGGER.error(ex.getMessage());
+                    }
+                } else {
+                    world.getMaps().forEach(map -> map.getMarkerSets().put(bannerMarkerSetId, MarkerSet.builder().label("Map Banners").defaultHidden(false).toggleable(true).build()));
                 }
             }
-        })));
+        });
     }
 
-    public void removeMarker(BlockEntity blockEntity) {
-        toggleMarker(null, blockEntity);
+    public void saveMarkerSet(World mcWorld) {
+        String fileName = FabricLoader.getInstance().getConfigDir().resolve(String.format("banners4bm/maps/%s.json", worldToString(mcWorld))).toString();
+        BlueMapAPI.getInstance().flatMap(api -> api.getWorld(mcWorld)).ifPresent(world -> {
+            BlueMapMap map = world.getMaps().iterator().next();
+            map.getMarkerSets().forEach((id, markerSet) -> {
+                if (id != null && id.equals(bannerMarkerSetId)) {
+                    try (FileWriter writer = new FileWriter(fileName)) {
+                        MarkerGson.INSTANCE.toJson(markerSet, writer);
+                    } catch (IOException ex) {
+                        LOGGER.error(ex.getMessage());
+                    }
+                }
+            });
+        });
     }
 
-    public void toggleMarker(BlockState blockState, BlockEntity blockEntity) {
-        if (!(blockEntity instanceof BannerBlockEntity bannerBlockEntity)) {
-            return;
-        }
-        BlueMapAPI.getInstance().flatMap(blueMapAPI -> blueMapAPI.getWorld(blockEntity.getWorld())).ifPresent(blueMapWorld -> {
+    public void removeMarker(BannerBlockEntity bannerBlockEntity) {
+        toggleMarker(bannerBlockEntity, null);
+    }
+
+    public void toggleMarker(BannerBlockEntity bannerBlockEntity, BlockState blockState) {
+        BlueMapAPI.getInstance().flatMap(blueMapAPI -> blueMapAPI.getWorld(bannerBlockEntity.getWorld())).ifPresent(blueMapWorld -> {
             blueMapWorld.getMaps().forEach(blueMapMap -> {
                 var existingBannerMarkerSet = blueMapMap.getMarkerSets().get(bannerMarkerSetId);
                 if (existingBannerMarkerSet == null) {
                     return;
                 }
-                var markerId = blockEntity.getPos().toShortString();
+                var markerId = bannerBlockEntity.getPos().toShortString();
                 var existingMarker = existingBannerMarkerSet.getMarkers().get(markerId);
                 if (existingMarker != null) {
                     existingBannerMarkerSet.remove(markerId);
@@ -91,19 +84,28 @@ public class BannerMarkerManager {
                         var blockTranslationKey = blockState.getBlock().getTranslationKey();
                         name = Text.translatable(blockTranslationKey).getString();
                     }
-                    addMarker(name, bannerBlockEntity, existingBannerMarkerSet, blueMapMap);
+                    addMarker(bannerBlockEntity, name, existingBannerMarkerSet, blueMapMap);
                 }
             });
         });
+        saveMarkerSet(bannerBlockEntity.getWorld());
     }
 
-    private void addMarker(String blockName, BannerBlockEntity bannerBlockEntity, MarkerSet existingBannerMarkerSet, BlueMapMap blueMapMap) {
-        var blockPos = bannerBlockEntity.getPos();
-        var x = blockPos.toCenterPos().getX();
-        var y = blockPos.toCenterPos().getY();
-        var z = blockPos.toCenterPos().getZ();
-        var iconAddress = blueMapMap.getAssetStorage().getAssetUrl(bannerBlockEntity.getColorForState().name().toLowerCase() + ".png");
-        POIMarker bannerMarker = POIMarker.builder().label(blockName).position(x, y, z).icon(iconAddress, 0, 0).build();
-        existingBannerMarkerSet.put(blockPos.toShortString(), bannerMarker);
+    private void addMarker(BannerBlockEntity bannerBlockEntity, String blockName, MarkerSet markerSet, BlueMapMap map) {
+        Vec3d pos = bannerBlockEntity.getPos().toCenterPos();
+        var iconAddress = map.getAssetStorage().getAssetUrl(bannerBlockEntity.getColorForState().name().toLowerCase() + ".png");
+        POIMarker bannerMarker = POIMarker.builder().label(blockName).position(pos.getX(), pos.getY(), pos.getZ()).icon(iconAddress, 0, 0).build();
+        markerSet.put(bannerBlockEntity.getPos().toShortString(), bannerMarker);
+    }
+
+    // Pretty sure there is a better way, but I don't know it... Maybe you know?
+    private String worldToString(World world) {
+        String string = world.getRegistryKey().toString().replace("ResourceKey[minecraft:dimension / minecraft:", "").replace("]", "");
+        if (string.equals("overworld")) {
+            string = "";
+        } else {
+            string = "_" + string;
+        }
+        return  world.toString().replace("ServerLevel[", "").replace("]", "") + string;
     }
 }
--- src/main/java/dev/nincodedo/banners4bm/Banners4BM.java
+++ src/main/java/dev/nincodedo/banners4bm/Banners4BM.java
@@ -2,16 +2,23 @@ package dev.nincodedo.banners4bm;
 
 import de.bluecolored.bluemap.api.BlueMapAPI;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerBlockEntityEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BannerBlockEntity;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
 import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -39,24 +46,31 @@ public class Banners4BM implements ModInitializer {
             }
             BlueMapAPI.onEnable(blueMapAPI -> {
                 LOGGER.info("Integrating Banners4BM into BlueMap");
-                bannerMarkerManager.loadMarkers(server.getOverworld());
+                bannerMarkerManager.loadMarkerSets();
                 bannerMapIcons.loadMapIcons(blueMapAPI);
             });
         });
         BlueMapAPI.onDisable(blueMapAPI -> {
             LOGGER.info("Stopping Banners4BM");
-            bannerMarkerManager.saveMarkers();
         });
-        UseBlockCallback.EVENT.register(this::mapBannerInteraction);
-        PlayerBlockBreakEvents.AFTER.register((world, player, pos, state, blockEntity) -> {
-            if (world.getRegistryKey().equals(World.OVERWORLD) && state.isIn(BlockTags.BANNERS)) {
-                bannerMarkerManager.removeMarker(blockEntity);
-            }
-        });
+        UseBlockCallback.EVENT.register(this::useBlock);
+        PlayerBlockBreakEvents.AFTER.register(this::breakBlock);
+        //ServerBlockEntityEvents.BLOCK_ENTITY_UNLOAD.register(this::blockEntityUnload);
     }
 
-    private ActionResult mapBannerInteraction(PlayerEntity player, World world, Hand hand, BlockHitResult hitResult) {
-        if (!world.getRegistryKey().equals(World.OVERWORLD) || player.isSpectator() || player.getMainHandStack().isEmpty() && player.getOffHandStack().isEmpty()) {
+//    private void blockEntityUnload(BlockEntity blockEntity, ServerWorld serverWorld) {
+//
+//        LOGGER.info("unload block entity");
+//        LOGGER.info(String.valueOf(serverWorld.getServer().isStopping()));
+//        LOGGER.info(String.valueOf(serverWorld.getServer().isPaused()));
+//        LOGGER.info(String.valueOf(serverWorld.getServer().isSaving()));
+//        if (blockEntity.getType() == BlockEntityType.BANNER && !serverWorld.getServer().isStopping() && !serverWorld.getServer().isPaused() && !serverWorld.getServer().isSaving()) {
+//            bannerMarkerManager.removeMarker((BannerBlockEntity) blockEntity);
+//        }
+//    }
+
+    private ActionResult useBlock(PlayerEntity player, World world, Hand hand, BlockHitResult hitResult) {
+        if (player.isSpectator() || player.getMainHandStack().isEmpty() && player.getOffHandStack().isEmpty()) {
             return ActionResult.PASS;
         }
         if (player.getMainHandStack().isOf(Items.FILLED_MAP) || player.getOffHandStack().isOf(Items.FILLED_MAP)) {
@@ -64,9 +78,15 @@ public class Banners4BM implements ModInitializer {
             var blockEntity = world.getBlockEntity(hitResult.getBlockPos());
             if (blockState != null && blockState.isIn(BlockTags.BANNERS)) {
                 LOGGER.trace("Toggling marker at {}", hitResult.getBlockPos());
-                bannerMarkerManager.toggleMarker(blockState, blockEntity);
+                bannerMarkerManager.toggleMarker((BannerBlockEntity) blockEntity, blockState);
             }
         }
         return ActionResult.PASS;
+    }
+
+    private void breakBlock(World world, PlayerEntity player, BlockPos pos, BlockState state, BlockEntity blockEntity) {
+        if (state.isIn(BlockTags.BANNERS)) {
+            bannerMarkerManager.removeMarker((BannerBlockEntity) blockEntity);
+        }
     }
 }
