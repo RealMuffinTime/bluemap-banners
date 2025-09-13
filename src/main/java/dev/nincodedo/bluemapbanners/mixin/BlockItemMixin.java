93554ad5ab28341c99a40a393278fb938eaefed1
RealMuffinTime <realmuffintime@gmail.com>
2025-09-13 22:15:52 +0200

    fix custom name not being saved when using markerAddInstantOnBannerPlace closes #7

--- src/main/java/dev/nincodedo/bluemapbanners/mixin/BlockItemMixin.java
+++ src/main/java/dev/nincodedo/bluemapbanners/mixin/BlockItemMixin.java
@@ -6,11 +6,12 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BannerBlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItem;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.text.ClickEvent;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -22,8 +23,13 @@ import java.net.URI;
 
 @Mixin(BlockItem.class)
 public class BlockItemMixin {
-    @Inject(at = @At("HEAD"), method = "postPlacement")
-    public void onPlaceInject(BlockPos pos, World world, PlayerEntity player, ItemStack stack, BlockState state, CallbackInfoReturnable<Boolean> cir) {
+    @Inject(method = "place(Lnet/minecraft/item/ItemPlacementContext;)Lnet/minecraft/util/ActionResult;",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;emitGameEvent(Lnet/minecraft/registry/entry/RegistryEntry;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/world/event/GameEvent$Emitter;)V"))
+    public void onPlaceInject(ItemPlacementContext context, CallbackInfoReturnable<ActionResult> cir) {
+        BlockPos pos = context.getBlockPos();
+        World world = context.getWorld();
+        PlayerEntity player = context.getPlayer();
+        BlockState state = world.getBlockState(pos);
         if (state.isIn(BlockTags.BANNERS)) {
             MarkerManager markerManager = MarkerManager.getInstance();
             ConfigManager configManager = ConfigManager.getInstance();
@@ -38,11 +44,11 @@ public class BlockItemMixin {
                         name = Text.translatable(blockTranslationKey).getString();
                     }
                     markerManager.addMarker(bannerBlockEntity, name);
-                    if (configManager.getBoolConfig("notifyPlayerOnMarkerAdd"))
+                    if (configManager.getBoolConfig("notifyPlayerOnMarkerAdd") && player != null)
                         player.sendMessage(Text.literal("[BlueMap Banners] You added a marker to the ").append(Text.literal("web map").setStyle(Style.EMPTY.withClickEvent(new ClickEvent.OpenUrl(URI.create(ConfigManager.getInstance().getConfig("bluemapUrl")))).withUnderline(true))).append(Text.of(".")), false);
                 }
             } else {
-                if (configManager.getBoolConfig("notifyPlayerOnBannerPlace")) {
+                if (configManager.getBoolConfig("notifyPlayerOnBannerPlace") && player != null) {
                     player.sendMessage(Text.literal("[BlueMap Banners] Use a map item on the banner to add a marker on the ").append(Text.literal("web map").setStyle(Style.EMPTY.withClickEvent(new ClickEvent.OpenUrl(URI.create(ConfigManager.getInstance().getConfig("bluemapUrl")))).withUnderline(true))).append(Text.of(".")), false);
                 }
             }
